7843f83b1b8aee07ed7d7931d3f0621f9d24ffef
thereclaimer <sam.davenport@thereclaimergame.com>
2024-01-29 12:37:32 -0600

    added the gruvbox color scheme

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -16,8 +16,4 @@ local plugins = "plugins"
 -- Example using a list of specs with the default options
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
-require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-})
+require("lazy").setup('plugins')
