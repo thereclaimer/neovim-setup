0cc21f29bc4801a947255b7b41ef68ae2628b64d
thereclaimer <sam.davenport@thereclaimergame.com>
2024-02-05 07:51:48 -0600

    more styling

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,8 @@ local opts = { noremap = true, silent = true }
 --normal maps 
 vim.keymap.set("n","<leader>e",":NvimTreeFocus<CR>",opts)
 vim.keymap.set("n","<leader>t",":NvimTreeToggle<CR>",opts)
+vim.keymap.set("n","<leader>v",":vsplit<CR>",opts)
+vim.keymap.set("n","<leader>w","<C-w>w",opts)
 
 --insert maps
 vim.keymap.set("i","<M-[>","<Esc>")
